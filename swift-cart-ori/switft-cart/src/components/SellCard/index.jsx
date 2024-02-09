9f2279c16b3569ad41f2a17e2412bb377c715b93
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-02-09 11:20:21 -0300

    Order by Popularity added in Main Page

--- swift-cart-ori/switft-cart/src/components/SellCard/index.jsx
+++ swift-cart-ori/switft-cart/src/components/SellCard/index.jsx
@@ -5,34 +5,44 @@ import { GiMoneyStack } from "react-icons/gi";
 import { Link } from "react-router-dom";
 
 export default function SellCard({ products }) {
+  // const popularProducts = [...products];
+
+  // popularProducts.sort((a, b) => b.stars - a.stars);
+
   return (
     <>
-      {products.map((product, id) => (
-        <div className="sell-card" key={id}>
-          <Link to={`/${product.id}`}>
-            <img src={product.image} className="sell-card-img" alt="Product" />
-          </Link>
-          <h1>{product.name}</h1>
-          <p className="product-desc">{product.description}</p>
-          <div className="price-wrapper">
-            <p className="price">R$ {product.price}</p>
-            <p>
-              <FaStar />
-              {product.stars}
-            </p>
+      {products
+        .sort((a, b) => b.stars - a.stars)
+        .map((product, id) => (
+          <div className="sell-card" key={id}>
+            <Link to={`/${product.id}`}>
+              <img
+                src={product.image}
+                className="sell-card-img"
+                alt="Product"
+              />
+            </Link>
+            <h1>{product.name}</h1>
+            <p className="product-desc">{product.description}</p>
+            <div className="price-wrapper">
+              <p className="price">R$ {product.price}</p>
+              <p>
+                <FaStar />
+                {product.stars}
+              </p>
+            </div>
+            <div className="sell-btn-wrapper">
+              <button className="cart-btn">
+                <BsCart4 />
+                Carrinho
+              </button>
+              <button className="buy-btn">
+                <GiMoneyStack />
+                Comprar
+              </button>
+            </div>
           </div>
-          <div className="sell-btn-wrapper">
-            <button className="cart-btn">
-              <BsCart4 />
-              Carrinho
-            </button>
-            <button className="buy-btn">
-              <GiMoneyStack />
-              Comprar
-            </button>
-          </div>
-        </div>
-      ))}
+        ))}
     </>
   );
 }

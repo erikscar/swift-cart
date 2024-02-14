878232ad2af879f57f938fa521da61f8a5e67495
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-02-14 19:15:14 -0300

    added wishlist page

--- swift-cart-ori/switft-cart/src/pages/WishList/index.jsx
+++ swift-cart-ori/switft-cart/src/pages/WishList/index.jsx
@@ -1,6 +1,8 @@
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
-import { FaHeart } from "react-icons/fa6";
+import { FaHeart, FaTrash } from "react-icons/fa6";
+import { BsCart4 } from "react-icons/bs";
+
 import "./index.css";
 export default function WishList() {
   return (
@@ -10,21 +12,27 @@ export default function WishList() {
         LISTA DE FAVORITOS <FaHeart className="heart-icon" />
       </h1>
 
-      <table className="fav-table">
+      <table cellSpacing="50">
         <thead>
           <tr>
-            <th>Nome</th>
-            <th>Preço</th>
+            <th>Produto</th>
             <th>Estoque</th>
+            <th>Preço</th>
             <th></th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td className="product-image-wrapper">
+              <img src="/apple.png" />
+              <p>Iphone</p>
+            </td>
+            <td>TUF Gaming</td>
+            <td>1</td>
+            <td>
+              <button className="add-btn">Adicionar ao Carrinho </button>
+              <button className="rmv-fav-btn">Remover dos Favoritos</button>
+            </td>
           </tr>
         </tbody>
       </table>

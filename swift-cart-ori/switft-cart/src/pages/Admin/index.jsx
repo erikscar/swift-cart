73be311c4d7c29fdd5753cf70e5d83f566b1afc7
Erik Scarcela Araujo <erikscarcela@gmail.com>
2024-02-28 14:45:59 -0300

    added get products in admin page

--- swift-cart-ori/switft-cart/src/pages/Admin/index.jsx
+++ swift-cart-ori/switft-cart/src/pages/Admin/index.jsx
@@ -1,9 +1,46 @@
-
+import { useEffect, useState } from "react"
+import "./index.css"
+import axios from "axios"
+import SellCard from "../../components/SellCard"
 const Admin = () => {
+    const [products, setProducts] = useState([])
+
+    const getProducts = async () => {
+        try {
+            const res = await axios.get("http://localhost:8800/products")
+            setProducts(res.data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const postProduct = async () => {
+        try {
+            await axios.post("http://localhost:8800/products", {
+
+            })
+        } catch (error) {
+
+        }
+    }
+
+    useEffect(() => {
+        getProducts()
+    }, [setProducts])
+
     return (
-        <div>
-            <p>DSADSADASDSD</p>
-        </div>
+        <>
+            <div className="logo">
+                <img src="/logo.png" alt="logo-img" className="logo-img" />
+                <h1>SwiftCart</h1>
+            </div>
+            <div className="admin-container">
+                <button className="add-product-btn">Adicionar Produto</button>
+                <div className="sell-card-container">
+                    <SellCard products={products} />
+                </div>
+            </div>
+        </>
     )
 }
 
